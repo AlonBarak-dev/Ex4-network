bee644485d8e509b44841d2a81008e9c8a5d36fc
Alon Barak <alonbarak3110@gmail.com>
2021-12-15 13:11:02 +0200

    switching socket algorithms works

--- measure.c
+++ measure.c
@@ -7,13 +7,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <time.h>
-#define SIZE 1024
+#define SIZE 256
  
 float write_file(int sock){
     int n;
     FILE *fp;
     char *filename = "recv.txt";
     char buffer[SIZE];
+    bzero(buffer, SIZE);
     float time = 0;
     clock_t before = clock();
     fp = fopen(filename, "w");
@@ -32,120 +33,239 @@ float write_file(int sock){
     return time;
 }
  
-int main(){
-  char *ip = "127.0.0.1";
-  int port = 8080;
-  int e;
-  float time_cubic = 0;
-  float time_reno = 0; 
- 
-//-----------------------------------------------------------------------
-
-char buf[SIZE];
-socklen_t len;
-int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-if (sock == -1)
+int main ()
 {
-    perror("socket");
-    return -1;
+    
+        
+        char *ip = "127.0.0.1";
+        int port = 80;
+        int e;
+
+        int sockfd, new_sock;
+        struct sockaddr_in server_addr, new_addr;
+        socklen_t addr_size;
+        char buffer[SIZE];
+        char buf[SIZE];
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(sockfd<0)
+        {
+            perror("[-]Error in socket");
+            exit(1);
+        }
+        printf("[+]Server socket created. \n");
+
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = port;
+        server_addr.sin_addr.s_addr = inet_addr(ip);
+
+        e = bind(sockfd,(struct sockaddr*)&server_addr, sizeof(server_addr));
+        if(e<0)
+        {
+            perror("[-]Error in Binding");
+            exit(1);
+        }
+        printf("[+]Binding Successfull.\n");
+    printf("Current: %s\n", buf);
+    float avg = 0;
+    float sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        time_t before = clock();        // starts counting
+        e = listen(sockfd, 10);
+        if(e==0)
+        {
+            printf("[+]Listening...\n");
+        }
+        else 
+        {
+            perror("[-]Error in Binding");
+            exit(1);
+        }
+        addr_size = sizeof(new_addr);
+        new_sock = accept(sockfd,(struct sockaddr*)&new_addr, &addr_size);
+
+        write_file(new_sock);
+        printf("[+]Data written in the text file \n");
+        time_t delta = clock() - before;
+        sum += delta;
+    }
+    sum = sum *1000 / CLOCKS_PER_SEC;
+    avg = sum / 5;
+    printf("Cubic times: %f\n",avg);
+
+    strcpy(buf,"reno");     // reno
+    socklen_t len = strlen(buf);
+
+    if(setsockopt(sockfd,IPPROTO_TCP,TCP_CONGESTION,buf, len)!=0){
+        perror("getsocket");
+        return -1;
+    }
+    printf("New: %s \n",buf);
+
+    avg = 0;
+    sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        time_t before = clock();        // starts counting
+        e = listen(sockfd, 10);
+        if(e==0)
+        {
+            printf("[+]Listening...\n");
+        }
+        else 
+        {
+            perror("[-]Error in Binding");
+            exit(1);
+        }
+        addr_size = sizeof(new_addr);
+        new_sock = accept(sockfd,(struct sockaddr*)&new_addr, &addr_size);
+
+        write_file(new_sock);
+        printf("[+]Data written in the text file ");
+        time_t delta = clock() - before;
+        sum += delta;
+    }
+    sum = sum *1000 / CLOCKS_PER_SEC;
+    avg = sum / 5;
+    printf("Reno times: %f\n",avg);
+
+
+
+
+
+
+
+
+    return 0;
 }
 
-len = sizeof(buf);
 
-if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
-{
-    perror("getsockopt");
-    return -1;
-}
 
-printf("Current: %s\n", buf);
 
-//-----------------------------------------------------------------------
 
-  int new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  char buffer[SIZE];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if (sock == -1)
+// {
+//     perror("socket");
+//     return -1;
+// }
+
+// len = sizeof(buf);
+
+// if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
+// {
+//     perror("getsockopt");
+//     return -1;
+// }
+
+// printf("Current: %s\n", buf);
+
+// //-----------------------------------------------------------------------
+
+//   int new_sock;
+//   struct sockaddr_in server_addr, new_addr;
+//   socklen_t addr_size;
+//   char buffer[SIZE];
  
-//   sock = socket(AF_INET, SOCK_STREAM, 0);
-//   if(sock < 0) {
-//     perror("[-]Error in socket");
+// //   sock = socket(AF_INET, SOCK_STREAM, 0);
+// //   if(sock < 0) {
+// //     perror("[-]Error in socket");
+// //     exit(1);
+// //   }
+//   printf("[+]Server socket created successfully.\n");
+ 
+//   server_addr.sin_family = AF_INET;
+//   server_addr.sin_port = port;
+//   server_addr.sin_addr.s_addr = inet_addr(ip);
+ 
+//   e = bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+//   if(e < 0) {
+//     perror("[-]Error in bind");
 //     exit(1);
 //   }
-  printf("[+]Server socket created successfully.\n");
+//   printf("[+]Binding successfull.\n");
  
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
- 
-  e = bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e < 0) {
-    perror("[-]Error in bind");
-    exit(1);
-  }
-  printf("[+]Binding successfull.\n");
- 
-  if(listen(sock, 10) == 0){
-    printf("[+]Listening....\n");
-  }
-  else{
-    perror("[-]Error in listening");
-    exit(1);
-  }
+//   if(listen(sock, 10) == 0){
+//     printf("[+]Listening....\n");
+//   }
+//   else{
+//     perror("[-]Error in listening");
+//     exit(1);
+//   }
 
-addr_size = sizeof(new_addr);
-new_sock = accept(sock, (struct sockaddr*)&new_addr, &addr_size);
+// addr_size = sizeof(new_addr);
+// new_sock = accept(sock, (struct sockaddr*)&new_addr, &addr_size);
 
  
-time_cubic += write_file(new_sock);
-printf("%f",time_cubic);
-printf("[+]Data written in the file successfully in cubic.\n");
-close(sock);
+// time_cubic += write_file(new_sock);
+// printf("%f",time_cubic);
+// printf("[+]Data written in the file successfully in cubic.\n");
+// close(sock);
 
-//------------------------------------
+// //------------------------------------
 
-sock = socket(AF_INET, SOCK_STREAM, 0);
-strcpy(buf, "reno");
+// sock = socket(AF_INET, SOCK_STREAM, 0);
+// strcpy(buf, "reno");
 
-len = strlen(buf);
+// len = strlen(buf);
 
-if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0)
-{
-    perror("setsockopt");
-    return -1;
-}
+// if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0)
+// {
+//     perror("setsockopt");
+//     return -1;
+// }
 
-len = sizeof(buf);
+// len = sizeof(buf);
 
-if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
-{
-    perror("getsockopt");
-    return -1;
-}
+// if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
+// {
+//     perror("getsockopt");
+//     return -1;
+// }
 
-printf("New: %s\n", buf);
-//------------------------------------
+// printf("New: %s\n", buf);
+// //------------------------------------
 
-  if(listen(sock, 10) == 0){
-    printf("[+]Listening....\n");
-  }
-  else{
-    perror("[-]Error in listening");
-    exit(1);
-  }
+//   if(listen(sock, 10) == 0){
+//     printf("[+]Listening....\n");
+//   }
+//   else{
+//     perror("[-]Error in listening");
+//     exit(1);
+//   }
 
-addr_size = sizeof(new_addr);
-new_sock = accept(sock, (struct sockaddr*)&new_addr, &addr_size);
+// addr_size = sizeof(new_addr);
+// new_sock = accept(sock, (struct sockaddr*)&new_addr, &addr_size);
 
-time_reno += write_file(new_sock);
-printf("%f",time_reno);
-printf("[+]Data written in the file successfully in reno.\n");
-
-
+// time_reno += write_file(new_sock);
+// printf("%f",time_reno);
+// printf("[+]Data written in the file successfully in reno.\n");
 
 
 
-close(sock);
-return 0;
-}
+
+
+// close(sock);
+// return 0;
--- sender.c
+++ sender.c
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-#define SIZE 1024
+#define SIZE 256
  
 void send_file(FILE *fp, int sock){
   //int n;
@@ -20,117 +20,249 @@ void send_file(FILE *fp, int sock){
   }
 }
  
+
+
 int main(){
-  char *ip = "127.0.0.1";
-  int port = 8080;
-  int e;
+  
+for (int i = 0; i < 5; i++)
+{
+    
+    char *ip = "127.0.0.1";
+    int port = 80;
+    int e;
+
+    int sockfd;
+    struct sockaddr_in server_addr;
+    FILE *fp;
+    char *filename = "1000Nodes.json";
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd<0)
+    {
+        perror("[-]Error in socket");
+        exit(1);
+    }
+     printf("[+]Server socket created. \n");
+
+     server_addr.sin_family = AF_INET;
+     server_addr.sin_port = port;
+     server_addr.sin_addr.s_addr = inet_addr(ip);
+
+     e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+     if(e == -1)
+     {
+         perror("[-]Error in Connecting");
+         exit(1);
+     }
+     printf("[+]Connected to server.\n");
+     fp = fopen(filename, "r");
+     if(fp == NULL)
+     {
+         perror("[-]Error in reading file.");
+         exit(1);
+     }
+     send_file(fp,sockfd);
+     printf("[+] File data send successfully. \n");
+     close(sockfd);
+     printf("[+]Disconnected from the server. \n");
+
+    close(sockfd);
+}
 
 
-  //-------------------------------------
 
+for (int i = 0; i < 5; i++)
+{
+    
+    char *ip = "127.0.0.1";
+    int port = 80;
+    int e;
     char buf[SIZE];
     socklen_t len;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sock == -1)
+    int sockfd;
+    struct sockaddr_in server_addr;
+    FILE *fp;
+    char *filename = "1000Nodes.json";
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd<0)
     {
-        perror("socket");
-        return -1;
+        perror("[-]Error in socket");
+        exit(1);
     }
+     printf("[+]Server socket created. \n");
+    
+     server_addr.sin_family = AF_INET;
+     server_addr.sin_port = port;
+     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    len = sizeof(buf);
-
-    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
-    {
-        perror("getsockopt");
-        return -1;
-    }
-
-    printf("Current: %s\n", buf);
-
-  //-------------------------------------
- 
-  struct sockaddr_in server_addr;
-  FILE *fp;
-  char *filename = "1000Nodes.json";
- 
-  
-  printf("[+]Server socket created successfully.\n");
-  
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
- 
-  e = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e == -1) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
- printf("[+]Connected to Server.\n");
- 
-  fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("[-]Error in reading file.");
-    exit(1);
-  }
-  printf("%d", sock);
-  for (int i = 0; i < 5; i++)
-  {
-    send_file(fp, sock);
-  }
-  printf("[+]File data sent successfully in cubic.\n");
-
-    close(sock);
-  //--------------------------------------------
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    strcpy(buf, "reno");
-
+    strcpy(buf,"reno");
     len = strlen(buf);
-
-    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0)
-    {
+    if(setsockopt(sockfd,IPPROTO_TCP,TCP_CONGESTION,buf,len) != 0){
         perror("setsockopt");
         return -1;
     }
+    printf("New: %s",buf);
 
-    len = sizeof(buf);
+     e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+     if(e == -1)
+     {
+         perror("[-]Error in Connecting");
+         exit(1);
+     }
+     printf("[+]Connected to server.\n");
+     fp = fopen(filename, "r");
+     if(fp == NULL)
+     {
+         perror("[-]Error in reading file.");
+         exit(1);
+     }
+     send_file(fp,sockfd);
+     printf("[+] File data send successfully. \n");
+     close(sockfd);
+     printf("[+]Disconnected from the server. \n");
 
-    if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
-    {
-        perror("getsockopt");
-        return -1;
-    }
-
-    printf("New: %s\n", buf);
-
-  //--------------------------------------------
-e = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e == -1) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
- printf("[+]Connected to Server.\n");
- 
-  fp = fopen(filename, "r");
-  if (fp == NULL) {
-    perror("[-]Error in reading file.");
-    exit(1);
-  }
-
-printf("%d", sock);
-for (int i = 0; i < 5; i++)
-  {
-      
-    send_file(fp, sock);
-  }
-  printf("[+]File data sent successfully in reno.\n");
-
-
-
-  printf("[+]Closing the connection.\n");
-  close(sock);
- 
-  return 0;
+    close(sockfd);
 }
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(){
+//   char *ip = "127.0.0.1";
+//   int port = 8080;
+//   int e;
+
+
+//   //-------------------------------------
+
+//     char buf[SIZE];
+//     socklen_t len;
+//     int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+//     if (sock == -1)
+//     {
+//         perror("socket");
+//         return -1;
+//     }
+
+//     len = sizeof(buf);
+
+//     if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
+//     {
+//         perror("getsockopt");
+//         return -1;
+//     }
+
+//     printf("Current: %s\n", buf);
+
+//   //-------------------------------------
+ 
+//   struct sockaddr_in server_addr;
+//   FILE *fp;
+//   char *filename = "1000Nodes.json";
+ 
+  
+//   printf("[+]Server socket created successfully.\n");
+  
+//   server_addr.sin_family = AF_INET;
+//   server_addr.sin_port = port;
+//   server_addr.sin_addr.s_addr = inet_addr(ip);
+ 
+//   e = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+//   if(e == -1) {
+//     perror("[-]Error in socket");
+//     exit(1);
+//   }
+//  printf("[+]Connected to Server.\n");
+ 
+//   fp = fopen(filename, "r");
+//   if (fp == NULL) {
+//     perror("[-]Error in reading file.");
+//     exit(1);
+//   }
+//   printf("%d", sock);
+//   for (int i = 0; i < 5; i++)
+//   {
+//     send_file(fp, sock);
+//   }
+//   printf("[+]File data sent successfully in cubic.\n");
+
+//     close(sock);
+//   //--------------------------------------------
+
+//     sock = socket(AF_INET, SOCK_STREAM, 0);
+
+//     strcpy(buf, "reno");
+
+//     len = strlen(buf);
+
+//     if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, len) != 0)
+//     {
+//         perror("setsockopt");
+//         return -1;
+//     }
+
+//     len = sizeof(buf);
+
+//     if (getsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, buf, &len) != 0)
+//     {
+//         perror("getsockopt");
+//         return -1;
+//     }
+
+//     printf("New: %s\n", buf);
+
+//   //--------------------------------------------
+// e = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+//   if(e == -1) {
+//     perror("[-]Error in socket");
+//     exit(1);
+//   }
+//  printf("[+]Connected to Server.\n");
+ 
+//   fp = fopen(filename, "r");
+//   if (fp == NULL) {
+//     perror("[-]Error in reading file.");
+//     exit(1);
+//   }
+
+// printf("%d", sock);
+// for (int i = 0; i < 5; i++)
+//   {
+      
+//     send_file(fp, sock);
+//   }
+//   printf("[+]File data sent successfully in reno.\n");
+
+
+
+//   printf("[+]Closing the connection.\n");
+//   close(sock);
+ 
+//   return 0;
